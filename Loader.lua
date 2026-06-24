acfaf88c1e1c234ae2ecd346cf3bc8335262c521
CloudyKermet <gamingbright2011@gmail.com>
2026-06-23 21:19:57 -0500

    Implement loading for Surf and Plunge
    Added loading functionality for Surf and Plunge game.

--- Loader.lua
+++ Loader.lua
@@ -3,3 +3,11 @@ if game.PlaceId == 93999763241813 then
     loadstring(game:HttpGet("https://pastebin.com/raw/9QMWnDm5"))()
 else 
 end
+
+--Surf and Plunge
+if game.PlaceId == 93041724457832 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudyKermet/Surf-and-Plunge/refs/heads/main/inf%20money"))()
+else 
+end
+
+
